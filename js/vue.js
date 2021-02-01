714be4d9b13e649b2048e92c9dc818873c087114
Federico <crudofederico@gmail.com>
2021-02-01 11:15:29 +0100

    header-left

---
+++ js/vue.js
@@ -0,0 +1,66 @@
+
+var print = new Vue({
+    el: '.prova',
+    data: {
+   
+    items:[
+        {
+            "index":0,
+            "user":"Luca",
+            "email":"luca@gmail.com",
+            "password":"luca",
+            "stato":"on",
+            
+        },
+        {
+            "index":1,
+            "user":"Luca",
+            "email":"luca@gmail.com",
+            "password":"luca",
+            "stato":"off"
+        },
+        {
+            "index":2,
+            "user":"Licci",
+            "email":"licci@gmail.com",
+            "password":"liccu",
+            "stato":"on"
+        },
+        {
+            "index":3,
+            "user":"Lorenzo",
+            "email":"lorenzo@gmail.com",
+            "password":"lore",
+            "stato":"on"
+        },
+        {
+            "index":4,
+            "user":"Marta",
+            "email":"lorenzo@gmail.com",
+            "password":"marta",
+            "stato":"off"
+        },
+        {
+            "index":5,
+            "user":"Federico",
+            "email":"federico@gmail.com",
+            "password":"federico",
+            "stato":"on"
+        },
+        {
+            "index":6,
+            "user":"Francesco",
+            "email":"francesco@gmail.com",
+            "password":"lore",
+            "stato":"off"
+        }
+    ]
+    },
+    methods:{
+        
+
+    }
+    
+  
+    
+    });
